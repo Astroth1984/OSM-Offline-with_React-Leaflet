79995c2ea69de5671475283cf612f90773a7af71
ElMounirOthmane <47123273+Astroth1984@users.noreply.github.com>
2023-11-15 12:52:39 +0100

    Set TileLayer url for internal node map server

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import './App.css';
 import "leaflet/dist/leaflet.css";
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
 import { Icon, divIcon, point } from 'leaflet';
 import MarkerClusterGroup from 'react-leaflet-cluster';
+
+
 
 function App() {
 
   //Add Markers
   const markers = [
     {
-      geocode: [50.88371959155463, 4.430784796696851],
-      popUp: "Eurocontrol, I work here"
+      geocode: [46.2097824357196, 6.139475957830832],
+      popUp: "I work here"
     },
     {
-      geocode: [50.8448954625438, 4.341385083201939],
+      geocode: [46.20066430916326, 6.141922132426539],
       popUp: "My Crush lives here. If you see her, say Hello!"
     },
     {
-      geocode: [50.887359986154515, 4.34168158017839],
+      geocode: [46.20033757285177, 6.166340963156938],
+      popUp : "This is a Marker pop-up"
+    },
+    {
+      geocode: [46.19190116548555, 6.163336889092037],
       popUp : "This is a Marker pop-up"
     }
   ];
@@ -28,19 +34,29 @@ function App() {
     iconSize: [45, 45] // size of the icon
   });
 
-  // this func should return a divIcon element
+  //this func should return a divIcon element
   const createCustomIconClusterIcon = (cluster) => {
     // Display what you want inside the Cluster Icon - In my case the number of markers inside the Cluster
     return new divIcon({
       html: `<div class="cluster-icon">${cluster.getChildCount()}</div>`,
       className: "custom-marker-cluster",
-      iconSize: point(33,33, true)
+      iconSize: point(25,25, true),
+      
     })
   }
+
+
   return (
-    <MapContainer center={[50.85045,4.34878]} zoom={13}>
+
+    <MapContainer 
+      center={[46.20482260019546, 6.14561285199199]} 
+      zoom={14}
+      maxZoom={16}
+      minZoom={11}
+      style={{ height: '100vh', width: '100%' }}
+    >
       <TileLayer 
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        url='http://localhost:3001/tiles/{z}/{x}/{y}.png'
       />
 
       <MarkerClusterGroup
@@ -59,6 +75,7 @@ function App() {
       </MarkerClusterGroup>
       
     </MapContainer>
+
   );
 }
 
