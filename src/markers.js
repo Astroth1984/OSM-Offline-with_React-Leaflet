3720cbabac6615d4dd2bd91644e152194ace0424
ElMounirOthmane <47123273+Astroth1984@users.noreply.github.com>
2023-11-17 12:23:42 +0100

    radius Fix

--- src/markers.js
+++ src/markers.js
@@ -2,25 +2,25 @@ const markers = [
     {
       geocode: [46.2097824357196, 6.139475957830832],
       popUp: "I work here",
-      range: 20,
+      range: 70,
       pathOptions: { fillColor: 'blue' }
     },
     {
       geocode: [46.20066430916326, 6.141922132426539],
       popUp: "My Crush lives here. If you see her, say Hello!",
-      range: 30,
+      range: 60,
       pathOptions: { color: 'lime' }
     },
     {
       geocode: [46.20033757285177, 6.166340963156938],
       popUp : "This is a Marker pop-up",
-      range : 50,
+      range : 70,
       pathOptions: { color: 'purple' }
     },
     {
       geocode: [46.19190116548555, 6.163336889092037],
       popUp : "This is a Marker pop-up",
-      range: 90,
+      range: 100,
       pathOptions: { color: 'green' }
     }
   ];
