9712da9d137b5f9a6aba6c8a6d7592cad2a1d7cb
ElMounirOthmane <47123273+Astroth1984@users.noreply.github.com>
2023-11-26 00:23:14 +0100

    Polar Grid Factory

--- src/containers/PolarGrid.js
+++ src/containers/PolarGrid.js
@@ -7,17 +7,6 @@ const PolarGrid = () => {
   const radiusOuter = 233 * 3;
   const radiusInter = 233 * 2;
   const radiusInner = 233;
-  const numberOfSectors = 16; // You can adjust this based on your preference for the number of sectors
-
-
-  // const pathOptions = {
-  //   stroke: 'false',
-  //   fillOpacity: '0.3',
-  //   opacity: '1',
-  //   weight: '1',
-  //   fillColor: 'rgb(0, 0, 0)',
-  //   color: 'rgb(0, 0, 0)',
-  // };
 
   const pathOptions = {
     stroke: 'false',
@@ -30,44 +19,75 @@ const PolarGrid = () => {
 
 
   const createCircle = (radius) => {
-    return <Circle center={center} radius={radius} pathOptions={pathOptions} />;
+    return <Circle 
+              center={center} 
+              radius={radius} 
+              pathOptions={pathOptions} 
+            />;
   };
 
   const createSectorLines = () => {
     const lines = [];
 
     // Adding a horizontal line passing through the center
-    const horizontalLine = <Polyline positions={[[center[0] - metersToLat(radiusOuter), center[1]], [center[0] + metersToLat(radiusOuter), center[1]] ]} pathOptions={pathOptions} />;
+    const horizontalLine = <Polyline 
+                              positions={
+                                [
+                                  [
+                                    center[0] - metersToLat(radiusOuter), 
+                                    center[1]
+                                  ], 
+                                  [
+                                    center[0] + metersToLat(radiusOuter), 
+                                    center[1]
+                                  ] 
+                                ]
+                              } 
+                              pathOptions={pathOptions} 
+                            />;
     lines.push(horizontalLine);
 
     // Adding a vertical line passing through the center
-    const verticalLine = <Polyline positions={[[center[0], center[1] - metersToLng(radiusOuter, center[0])], [center[0], center[1] + metersToLng(radiusOuter, center[0])]]} pathOptions={pathOptions} />;
+    const verticalLine = <Polyline 
+                            positions={
+                              [
+                                [
+                                  center[0], 
+                                  center[1] - metersToLng(radiusOuter, center[0])
+                                ], 
+                                [
+                                  center[0], 
+                                  center[1] + metersToLng(radiusOuter, center[0])
+                                ]
+                              ]
+                            } 
+                            pathOptions={pathOptions} 
+                          />
     lines.push(verticalLine);
 
     // Tang line
-    const tangLine = <Polyline positions={[calculateIntersection(center, radiusOuter, Math.PI/4), calculateIntersection(center, radiusOuter, Math.PI/4 + Math.PI)]} pathOptions={pathOptions} />
+    const tangLine = <Polyline 
+                        positions={
+                          [
+                            calculateIntersection(center, radiusOuter, Math.PI/4), 
+                            calculateIntersection(center, radiusOuter, Math.PI/4 + Math.PI)
+                          ]
+                        } 
+                        pathOptions={pathOptions} 
+                      />
     lines.push(tangLine);
 
 
-    const tangLineTwo = <Polyline positions={[calculateIntersection(center, radiusOuter, -Math.PI/4), calculateIntersection(center, radiusOuter, -Math.PI/4 + Math.PI)]} pathOptions={pathOptions} />
+    const tangLineTwo = <Polyline 
+                          positions={
+                            [
+                              calculateIntersection(center, radiusOuter, -Math.PI/4), 
+                              calculateIntersection(center, radiusOuter, -Math.PI/4 + Math.PI)
+                            ]
+                          } 
+                          pathOptions={pathOptions} 
+                        />
     lines.push(tangLineTwo);
-
-
-
-    // const tangLineThree = <Polyline positions={[calculateIntersection(center, radiusOuter, Math.PI/8), calculateIntersection(center, radiusOuter, Math.PI/8 + Math.PI)]} pathOptions={pathOptions} />
-    // lines.push(tangLineThree);
-
-
-    // const tangLineFour = <Polyline positions={[calculateIntersection(center, radiusOuter, -Math.PI/8), calculateIntersection(center, radiusOuter, -Math.PI/8 + Math.PI)]} pathOptions={pathOptions} />
-    // lines.push(tangLineFour);
-
-
-    // const tangLineFive = <Polyline positions={[calculateIntersection(center, radiusOuter, 3* Math.PI/4), calculateIntersection(center, radiusOuter, 3* Math.PI/4 + Math.PI)]} pathOptions={pathOptions} />
-    // lines.push(tangLineFive);
-
-
-    // const tangLineSix = <Polyline positions={[calculateIntersection(center, radiusOuter, -3* Math.PI/4), calculateIntersection(center, radiusOuter, -3* Math.PI/4 + Math.PI)]} pathOptions={pathOptions} />
-    // lines.push(tangLineSix);
 
     return lines;
   };
